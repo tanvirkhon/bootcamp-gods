1c1fb581128a7f02dd0a850f317e20aff7b5d629
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-02-16 14:57:00 -0600

    Added create Battle page and Loading screen

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import styles from "../styles";
+import { useGlobalContext } from "../context";
 
 // Import Higher Order Component for components folder
-import { PageHOC } from "../components";
+import { CustomButton, CustomInput, GameLoad, PageHOC } from "../components";
 
 const CreateBattle = () => {
+  const { contract, battleName, setBattleName, gameData } = useGlobalContext();
+  const [waitBattle, setWaitBattle] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (gameData?.activeBattle?.battleStatus === 0) {
+      setWaitBattle(true);
+    }
+  }, [gameData]);
+
+  const handleClick = async () => {
+    if (!battleName || !battleName.trim()) return null;
+
+    try {
+      await contract.createBattle(battleName);
+      setWaitBattle(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-white text-xl">Hello from CreateBattle</h1>
-    </div>
+    <>
+      {waitBattle && <GameLoad />}
+
+      <div className="flex flex-col mb-5">
+        <CustomInput
+          label="Battle"
+          placeholder="Enter Battle Name"
+          value={battleName}
+          handleValueChange={setBattleName}
+        />
+        <CustomButton
+          title="Create Battle"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
+
+      <p className={styles.infoText} onClick={() => navigate("/join-battle")}>
+        Or Join already existing battles
+      </p>
+    </>
   );
 };
 
@@ -16,7 +59,5 @@ export default PageHOC(
   <>
     Create <br /> a new Battle
   </>,
-  <>
-    Create your own battle and wait for other players to join you.
-  </>
+  <>Create your own battle and wait for other players to join you.</>
 );
